4feaaaa02a8f9be5bc5ae0232e9e99bc63c79a73
Nyarama <sylivestergeorge@gmail.com>
2023-10-20 10:58:25 +0300

    uploading footer

---
+++ footer/footer.php
@@ -0,0 +1,14 @@
+<!-- Footer Start -->
+<div class="container-fluid bg-primary footer mt-5 pt-5 wow fadeIn" data-wow-delay="0.1s">
+        <div class="container-fluid copyright">
+            <div class="container">
+                <div class="row">
+                    
+                    <div class="col-md-12 text-center text-md-end">
+                        Designed By <a href="https://acclavia.com">Acclavia Insurance Brokers</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer End -->
